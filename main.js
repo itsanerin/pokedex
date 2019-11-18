ebadcf4a4baac4b7b9ca3d9c809e3bae71192460
itsanerin <31420768+itsanerin@users.noreply.github.com>
2019-11-17 23:59:09 -0500

    inserted rest of pokemon, got rid of grid, absolutely positioned sprites in a sort of collage with trainer avatar. Got rid of header for the time being.

---
+++ main.js
@@ -0,0 +1,8 @@
+shadow = document.getElementById('shadow')
+avatar = document.getElementById('avatar')
+fadeIn = () => {
+    avatar.classList.remove('hide')
+    avatar.classList.add('show')
+  }
+
+// shadow.addEventListener('click', fadeIn())
